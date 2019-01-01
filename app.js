88dadd862900a76961855345d46458ca58053b20
NadimRai <rai_nadim@yahoo.com>
2019-01-02 01:17:18 +0800

    installed express

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const server = http.createServer((req,res) =>{
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Node Online Shop</title></head>');
-    res.write('<body><h1>Online Shop</h1></body>');
+    res.write('<body><h1>Online Shop </h1></body>');
     res.write('</html>');
     res.end();
 });
