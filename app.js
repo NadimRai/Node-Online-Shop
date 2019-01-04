9f2f9cc7c12b3a73e87e3b42266d9e9ab4c25b04
NadimRai <rai_nadim@yahoo.com>
2019-01-04 18:31:24 +0800

    converted html into ejs file

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views','views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
@@ -13,7 +16,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use('/',(req,res,next) =>{
-    res.status(404).sendFile(path.join(__dirname,'./','views','404.html'));
+    res.status(404).render('404',{pageTitle: 'Page Not Really Found'});
 });
 
 
