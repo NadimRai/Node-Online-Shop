8ea5707f7ef14d3fbadb367f8ea9b3f06cc96e4f
NadimRai <rai_nadim@yahoo.com>
2019-01-05 17:50:28 +0800

    ability to fetch data from files via model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+// const products = [];
+const fs = require('fs');
+const path = require('path');
+const productDir = require('../util/path');
 
 module.exports = class Product{
     constructor(title){
@@ -6,10 +9,28 @@ module.exports = class Product{
     }
 
     save(){
-        products.push(this);
+        // products.push(this);
+        const p = path.join(productDir, 'data','products.json');
+        fs.readFile(p, (err,filecontent) =>{
+            let products = [];
+            if(!err){
+                products = JSON.parse(filecontent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products),(err) =>{
+                console.log(err);
+            })
+        })
+        
     }
 
-    static fetchAll(){
-        return this.products;
+    static fetchAll(cb){
+        const p = path.join(productDir, 'data','products.json');
+       fs.readFile(p,(err, filecontent) =>{
+           if(err){
+               cb([]);
+           }
+           cb(JSON.parse(filecontent));
+       });
     }
 }
