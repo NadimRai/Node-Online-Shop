c4d909e69e40cad70db3b9053fd2ca89cbe804fd
NadimRai <rai_nadim@yahoo.com>
2019-01-05 14:47:28 +0800

    separated products logic in products controller

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use('/',(req,res,next) =>{
-    res.status(404).render('404',{pageTitle: 'Page Not Really Found'});
+    res.status(404).render('404',{path: '/',pageTitle: 'Page Not Really Found'});
 });
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,12 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-const rootDir = require('../util/path');
+// const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const products = [];
 
-router.get('/add-product',(req,res,next) =>{
-    res.render('add-product',{pageTitle: 'Add Product Page', path: '/admin/add-product'})
-
-});
+router.get('/add-product',productsController.getAddProduct);
 
 router.post('/add-product', (req,res,next) =>{
     products.push({title: req.body.title});
